3ab8baf52fe0a944473c85d82185021782af95c9
Lucio Maciel <luciofm@gmail.com>
2013-07-16 20:04:45 -0300

    Implementacao de ArrayAdapter...
    Signed-off-by: Lucio Maciel <luciofm@gmail.com>

--- src/com/luciofm/curso/ListViewActivityA.java
+++ src/com/luciofm/curso/ListViewActivityA.java
@@ -1,9 +1,15 @@
 package com.luciofm.curso;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ListViewActivityA extends Activity {
 
@@ -22,8 +28,69 @@ public class ListViewActivityA extends Activity {
 		setContentView(R.layout.list_activity);
 
 		list = (ListView) findViewById(R.id.list);
-		list.setAdapter(new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, android.R.id.text1,
+		list.setAdapter(new CidadesAdapter(this,
+				R.layout.new_list_item, R.id.text,
 				capitais));
+	}
+
+	public class CidadesAdapter extends ArrayAdapter<String> {
+
+		private int layout;
+		private int textId;
+		private LayoutInflater inflater;
+		
+		public CidadesAdapter(Context context, int resource,
+				int textViewResourceId, String[] objects) {
+			super(context, resource, textViewResourceId, objects);
+			layout = resource;
+			textId = textViewResourceId;
+			inflater = LayoutInflater.from(context);
+		}
+
+		/* Essa função retorna o número de Views (XML/Layouts)
+		 * diferentes que esse adapter utiliza...
+		 */
+		@Override
+		public int getViewTypeCount() {
+			return 2;
+		}
+
+		/* Essa função retorna qual o tipo da View baseado em sua 
+		 * posição...
+		 * Nesse caso, itens de indice par 0, itens impares 1
+		 */
+		@Override
+		public int getItemViewType(int position) {
+			return position % 2 == 0 ? 0 : 1;
+		}
+
+		/* Essa função retorna uma View, uma linha da nossa lista
+		 * Recebe a posição do item, uma view de cache (convertView),
+		 * e seu pai, a propria listview, ou gridview...
+		 */
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = convertView;
+
+			int viewType = getItemViewType(position) == 0 ? R.layout.new_list_item
+					: R.layout.new_list_item2;
+
+			if (v == null)
+				v = inflater.inflate(viewType, parent, false);
+
+			TextView textView = (TextView) v.findViewById(textId);
+			textView.setText(getItem(position));
+
+			/* Modificando em código.... */
+			/*if (position % 2 == 0) {
+				textView.setTextColor(Color.argb(255, 0, 0, 0));
+				v.setBackgroundColor(Color.WHITE);
+			} else {
+				textView.setTextColor(Color.WHITE);
+				v.setBackgroundColor(Color.BLACK);
+			}*/
+			
+			return v;
+		}
 	}
 }
